bc473d8226557cee659f8befaeb5fe61ff88b46d
Amune Israel <amuneisrael224@gmail.com>
2026-03-31 10:32:06 +0100

    update: typeorm db setup

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -11,21 +11,24 @@ import { AppController } from './app.controller';
       envFilePath: '.env',
     }),
  //   Database configuration (optional - uncomment when ready)
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST', 'localhost'),
-        port: configService.get<number>('DB_PORT', 5433),
-        username: configService.get<string>('DB_USERNAME', 'postgres'),
-        password: configService.get<string>('DB_PASSWORD', 'postgres'),
-        database: configService.get<string>('DB_NAME', 'cinemind'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: configService.get('NODE_ENV') !== 'production',
-        logging: configService.get('NODE_ENV') === 'development',
-      }),
-      inject: [ConfigService],
-    }),
+ TypeOrmModule.forRootAsync({
+  imports: [ConfigModule],
+  useFactory: (configService: ConfigService) => ({
+    type: 'postgres',
+    host: configService.get<string>('DB_HOST'),
+    port: configService.get<number>('DB_PORT'),
+    username: configService.get<string>('DB_USERNAME'),
+    password: configService.get<string>('DB_PASSWORD'),
+    database: configService.get<string>('DB_NAME'),
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: configService.get('NODE_ENV') !== 'production',
+    logging: configService.get('NODE_ENV') === 'development',
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }),
+  inject: [ConfigService],
+}),
   ],
   controllers: [AppController],
 })
